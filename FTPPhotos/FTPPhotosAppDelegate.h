31763af1deef43dc0954dca41fffce8e6b04d21c
Aaron McLeod <sircoolguy99@gmail.com>
2011-05-30 22:02:04 -0400

    Initial Commit

---
+++ FTPPhotos/FTPPhotosAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FTPPhotosAppDelegate.h
+//  FTPPhotos
+//
+//  Created by Aaron McLeod on 11-05-30.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTPPhotosAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ FTPPhotos/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FTPPhotos
+//
+//  Created by Aaron McLeod on 11-05-30.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
